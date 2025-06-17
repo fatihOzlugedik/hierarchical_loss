16dbaf66b0958697a7626ed5e1012dbdeadfd630
LAPTOP-KBFQJ636\fozlu <fatih.ozlugedik@tum.de>
2025-06-17 09:24:58 +0200

    baseline

---
+++ baseline/dataset_hiarachical.py
@@ -0,0 +1,94 @@
+import numpy as np
+import pickle
+from PIL import Image
+import torch
+from torch.utils.data import Dataset
+from torchvision import transforms
+import sys
+from sklearn.model_selection import StratifiedKFold
+import os
+import pandas as pd
+import h5py
+import hiarachical_loss as hl
+
+# Actual dataset class
+class MllDataset(Dataset):
+
+    '''MLL mil dataset class. Can be used by pytorch DataLoader '''
+
+    def __init__(
+            self,
+            path_of_dataset,
+            current_fold,
+            aug_im_order=True,
+            split=None):
+   
+        self.aug_im_order = aug_im_order
+        self.path_of_dataset = path_of_dataset
+        path_of_fold = os.path.join(path_of_dataset,f"data_fold_{current_fold}",f'{split}.csv')
+        data_of_fold = pd.read_csv(path_of_fold)
+        self.hiararchy ={
+        "root": ["Malignant", "NonMalignant"],
+
+        "Malignant": [
+        "Acute Leukemias",
+        "Myelodysplastic Syndromes",
+        "Myeloid Overlap Syndromes",
+        "Chronic Myeloid Neoplasms",
+        "Lymphoid Neoplasms",
+        "Plasma Cell Neoplasms"
+        ],
+        "Acute Leukemias": ["AML", "ALL", "AL"],
+        "Myelodysplastic Syndromes": ["MDS"],
+        "Myeloid Overlap Syndromes": ["CMML", "MDS / MPN", "MPN / MDS-RS-T"],
+        "Chronic Myeloid Neoplasms": ["MPN", "CML", "ET", "PV"],
+        "Lymphoid Neoplasms": ["B-cell neoplasm", "HCL", "T-cell neoplasm"],
+        "Plasma Cell Neoplasms": ["MM", "PCL"],
+
+        "NonMalignant": [
+        "Reactive Conditions",
+        "Normal Findings"
+        ],
+        "Reactive Conditions": ["Reactive changes"],
+        "Normal Findings": ["Normalbefund"]
+        }
+        self.hierarchical_loss = hl.HierarchicalLoss(self.hiararchy, device='cuda') 
+        self.leaf_to_idx = self.hierarchical_loss.leaf_to_idx
+        
+        self.patient = data_of_fold['patient_files'].tolist()
+        self.string_labels = data_of_fold['diagnose'].tolist()
+        self.labels = []
+        for label in self.string_labels:
+            if label in self.leaf_to_idx:
+                self.labels.append(self.leaf_to_idx[label])
+            else:
+                error_message = f"Label '{label}' not found in leaf_to_idx mapping."
+                raise ValueError(error_message)
+    
+        
+        
+    
+    
+
+    def __len__(self):
+        return len(self.labels)
+    
+        
+
+    def __getitem__(self, idx):
+
+        pat_id = self.patient[idx]
+        path= os.path.join(self.path_of_dataset, pat_id+'_combined.pt')
+        data = torch.load(path, map_location=torch.device("cpu"),weights_only=False)
+        bag = data['features']    
+        label = self.labels[idx]
+        # shuffle features by image order in bag, if desired
+        if(self.aug_im_order):
+            num_rows = bag.shape[0]
+            new_idx = torch.randperm(num_rows)
+            bag = bag[new_idx, :]
+
+
+        #label_regular = torch.Tensor([label]).long()
+
+        return bag, label, pat_id
---
+++ baseline/run_pipeline_hiararchical.py
@@ -0,0 +1,250 @@
+from model_train_hiarachical import *   # model training function
+from dataset_hiarachical import *       # dataset
+# makes conversion from string label to one-hot encoding easier
+from torchvision import transforms
+from torch.utils.data import Dataset, DataLoader, WeightedRandomSampler
+import torch.multiprocessing
+import torch
+import sys
+import os
+import time
+import argparse as ap
+import models
+from models.transformer import Transformer
+from models.wbcmil import WBCMIL
+import random
+
+torch.multiprocessing.set_sharing_strategy('file_system')
+
+
+
+
+
+# get arguments from parser, set up folder
+# parse arguments
+parser = ap.ArgumentParser()
+
+# Algorithm / training parameters
+parser.add_argument(
+    '--fold',
+    help='offset for cross-validation (1-5). Change to cross-validate',
+    required=True,
+    )  
+
+parser.add_argument(
+    '--arch',
+    required=True,
+    default='wbcmil',
+    choices = ['transformer', 'wbcmil'])  # shift folds for cross validation. Increasing by 1 moves all folds by 1.
+
+parser.add_argument(
+    '--lr',
+    help='used learning rate',
+    required=False,
+    default=0.00005)                                     # learning rate
+parser.add_argument(
+    '--ep',
+    help='max. amount after which training should stop',
+    required=False,
+    default=150)               # epochs to train
+parser.add_argument(
+    '--es',
+    help='early stopping if no decrease in loss for x epochs',
+    required=False,
+    default=20)          # epochs without improvement, after which training should stop.
+parser.add_argument(
+    '--multi_att',
+    help='use multi-attention approach',
+    required=False,
+    default=1)                          # use multiple attention values if 1
+
+# Data parameters: Modify the dataset
+parser.add_argument(
+    '--prefix',
+    help='define which set of features shall be used',
+    required=False,
+    default='fnl34_')        # define feature source to use (from different CNNs)
+# pass -1, if no filtering acc to peripheral blood differential count
+# should be done
+parser.add_argument(
+    '--filter_diff',
+    help='Filters AML patients with less than this perc. of MYB.',
+    default=20) #previously set to 20 
+# Leave out some more samples, if we have enough without them. Quality of
+# these is not good, but if data is short, still ok.
+parser.add_argument(
+    '--filter_mediocre_quality',
+    help='Filters patients with sub-standard sample quality',
+    default=0)
+parser.add_argument(
+    '--bootstrap_idx',
+    help='Remove one specific patient at pos X',
+    default=-
+    1)                             
+parser.add_argument(
+    '--result_folder',
+    help='store folder with custom name',
+    required=False)                                 
+parser.add_argument(
+    '--save_model',
+    help='choose wether model should be saved',
+    required=False,
+    default=1)
+parser.add_argument(
+    '--loss',
+    help='choose loss function',
+    required=True,
+    choices=['hl', 'CE'],
+    default=1)
+parser.add_argument(
+    '--alpha',
+    help='alpha value',
+    required=True,
+    default=1)            
+               # store model parameters if 1
+args = parser.parse_args()
+fold=args.fold
+loss=args.loss
+architecture = args.arch
+alpha = float(args.alpha)
+def seed_everything(seed=42):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)  # if you are using multi-GPU.
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    os.environ["PYTHONHASHSEED"] = str(seed)
+seed_everything(42) 
+
+dataset_path="/lustre/groups/labs/marr/qscd01/workspace/furkan.dasdelen/dino_feature_extractor/DinoBloom-vits14-features"
+TARGET_FOLDER=f"/lustre/groups/labs/marr/qscd01/workspace/fatih.oezluegedik/results/{loss}_{alpha}_{architecture}"
+# store results in target folder
+TARGET_FOLDER = os.path.join(TARGET_FOLDER, f'fold_{fold}')
+if not os.path.exists(TARGET_FOLDER):
+    os.makedirs(TARGET_FOLDER)
+start = time.time()
+
+datasets = {}
+
+datasets['train'] = MllDataset(
+  
+    path_of_dataset=dataset_path,
+    current_fold=int(fold),
+    aug_im_order=True,
+    split='train',
+)
+
+
+datasets['val'] = MllDataset(
+    path_of_dataset=dataset_path,
+    current_fold=int(fold),
+    aug_im_order=True,
+    split='val',
+)
+
+
+datasets['test'] = MllDataset(
+    path_of_dataset=dataset_path,
+    current_fold=int(fold),
+    aug_im_order=False,
+    split='test'
+)
+
+
+
+# Initialize dataloaders
+print("Initialize dataloaders...")
+dataloaders = {}
+
+
+
+print(datasets['train'])
+
+
+
+dataloaders['train'] = DataLoader(
+    datasets['train'],
+    num_workers=4  
+)
+
+dataloaders['val'] = DataLoader(
+datasets['val'],
+num_workers=4  
+)
+
+dataloaders['test'] = DataLoader(datasets['test'])
+print("")
+
+
+# 3: Model
+# initialize model, GPU link, training
+
+# set up GPU link and model (check for multi GPU setup)
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+ngpu = torch.cuda.device_count()
+print("Found device: ", ngpu, "x ", device)
+
+
+if args.arch.lower() == 'transformer':
+    model = Transformer(input_dim=384, num_classes=18, linear_dropout=0.0)
+elif args.arch.lower() == 'wbcmil':
+    model = WBCMIL(input_dim=384, num_classes=18, linear_dropout=0.0)
+
+
+if(ngpu > 1):
+    model = torch.nn.DataParallel(model)
+model = model.to(device)
+print("Setup complete.")
+print("")
+
+optimizer = optim.AdamW(model.parameters(), lr=float(args.lr), weight_decay=0.01)
+scheduler = None
+
+# launch training
+train_obj = ModelTrainer(
+    model=model,
+    dataloaders=dataloaders,
+    epochs=int(
+        args.ep),
+    optimizer=optimizer,
+    scheduler=scheduler,
+    class_count=18,
+    early_stop=int(
+        args.es),
+    device=device,
+    loss=loss,
+    alpha=alpha,
+    )
+model, conf_matrix, data_obj = train_obj.launch_training()
+
+
+# 4: aftermath
+# save confusion matrix from test set, all the data , model, print parameters
+
+np.save(os.path.join(TARGET_FOLDER, 'test_conf_matrix.npy'), conf_matrix)
+pickle.dump(
+    data_obj,
+    open(
+        os.path.join(
+            TARGET_FOLDER,
+            'testing_data.pkl'),
+        "wb"))
+
+if(int(args.save_model)):
+    torch.save(model, os.path.join(TARGET_FOLDER, 'model.pt'))
+    torch.save(model, os.path.join(TARGET_FOLDER, 'state_dictmodel.pt'))
+
+end = time.time()
+runtime = end - start
+time_str = str(int(runtime // 3600)) + "h" + str(int((runtime %
+                                                      3600) // 60)) + "min" + str(int(runtime % 60)) + "s"
+
+# other parameters
+print("")
+print("------------------------Final report--------------------------")
+print('prefix', args.prefix)
+print('Runtime', time_str)
+print('max. Epochs', args.ep)
+print('Learning rate', args.lr)
